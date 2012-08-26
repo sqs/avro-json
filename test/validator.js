da07814ade7cb4dfcfa67b702ecf5951d7bdd200
Quinn Slack <qslack@qslack.com>
2012-08-25 18:05:20 -0700

    test validation of primitive types

--- test/validator.js
+++ test/validator.js
@@ -38,6 +38,65 @@ exports['test'] = {
     test.throws(function() { return new Validator(123); });
     test.throws(function() { return new Validator(function() { }); });
     test.done();
-  }
+  },
 
+  // Primitive types
+  'null': function(test) {
+    test.ok(Validator.validate('null', null));
+    test.ok(Validator.validate('null', undefined));
+    test.strictEqual(Validator.validate('null', 1), false);
+    test.strictEqual(Validator.validate('null', 'a'), false);
+    test.done();
+  },
+  'boolean': function(test) {
+    test.ok(Validator.validate('boolean', true));
+    test.ok(Validator.validate('boolean', false));
+    test.strictEqual(Validator.validate('boolean', null), false);
+    test.strictEqual(Validator.validate('boolean', 1), false);
+    test.strictEqual(Validator.validate('boolean', 'a'), false);
+    test.done();
+  },
+  'int': function(test) {
+    test.ok(Validator.validate('int', 1));
+    test.ok(Validator.validate('long', Math.pow(2, 31) - 1));
+    test.strictEqual(Validator.validate('int', 1.5), false);
+    test.strictEqual(Validator.validate('int', Math.pow(2, 40)), false);
+    test.strictEqual(Validator.validate('int', null), false);
+    test.strictEqual(Validator.validate('int', 'a'), false);
+    test.done();
+  },
+  'long': function(test) {
+    test.ok(Validator.validate('long', 1));
+    test.ok(Validator.validate('long', Math.pow(2, 63) - 1));
+    test.strictEqual(Validator.validate('long', 1.5), false);
+    test.strictEqual(Validator.validate('long', Math.pow(2, 70)), false);
+    test.strictEqual(Validator.validate('long', null), false);
+    test.strictEqual(Validator.validate('long', 'a'), false);
+    test.done();
+  },
+  'float': function(test) {
+    test.ok(Validator.validate('float', 1));
+    test.ok(Validator.validate('float', 1.5));
+    test.strictEqual(Validator.validate('float', 'a'), false);
+    test.strictEqual(Validator.validate('float', null), false);
+    test.done();
+  },
+  'double': function(test) {
+    test.ok(Validator.validate('double', 1));
+    test.ok(Validator.validate('double', 1.5));
+    test.strictEqual(Validator.validate('double', 'a'), false);
+    test.strictEqual(Validator.validate('double', null), false);
+    test.done();
+  },
+  'bytes': function(test) {
+    // not implemented yet
+    test.throws(function() { Validator.validate('bytes', 1); });
+    test.done();
+  },
+  'string': function(test) {
+    test.ok(Validator.validate('string', 'a'));
+    test.strictEqual(Validator.validate('string', 1), false);
+    test.strictEqual(Validator.validate('string', null), false);
+    test.done();
+  }
 };
